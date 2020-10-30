5a54057a288b0e9d86764af661b129822d0ee52a
Kalovelo <apostoloskalovelonis@gmail.com>
2020-10-30 10:22:57 +0200

    add  more functional abstraction

--- project.m
+++ project.m
@@ -2,67 +2,59 @@ clear
 load iris.dat
 
 % Πρώτα 25 στοιχεία κάθε κλάσης
-training_set = iris(1:25,:)
-training_set = cat(1,iris(51:75,:),training_set)
-training_set = cat(1,iris(101:125,:),training_set)
+global training_set
+training_set = iris(1:25,:);
+training_set = cat(1,iris(51:75,:),training_set);
+training_set = cat(1,iris(101:125,:),training_set);
 
-testing_set = iris(26:50,:)
-testing_set = cat(1,iris(76:100,:),testing_set)
-testing_set = cat(1,iris(126:150,:),testing_set)
+global testing_set
+testing_set = iris(26:50,:);
+testing_set = cat(1,iris(76:100,:),testing_set);
+testing_set = cat(1,iris(126:150,:),testing_set);
 
-training_data = training_set(:,1:4)
-training_targets = training_set(:,5)
-testing_data = testing_set(:,1:4)
-testing_targets = testing_set(:,5)
+global training_data;
+global training_targets;
+global testing_data;
+global testing_targets;
+training_data = training_set(:,1:4);
+training_targets = training_set(:,5);
+testing_data = testing_set(:,1:4);
+testing_targets = testing_set(:,5);
 
-fis = fcm_fis(training_data,training_targets)
-[trainFis, trainError, stepSize, checkFis, checkError] = ...
-    anfis(training_set,fis,100,  [], testing_set);
-
-output = round(evalfis(trainFis,testing_data));
-
-get_success_results(output,testing_targets)
-[success_rate,successful] = get_success_results(output,testing_targets)
-
-plotClassification(output,testing_targets)
-
-showrule(fis)
-
-f = figure(2); %to show the 2 figures into different windows
-plotInputs(fis)
-
+global figure_index;
+figure_index=0;
+fcm();
+substractive();
+gridpartition();
 %------ Functions ------
 
-%fuzzy c-means
-function fis = fcm_fis(training_data,training_targets)
-opts = genfisOptions('FCMClustering');
-opts.NumClusters = 3;
-fis = genfis(training_data,training_targets,opts);
-end 
 
-
-%gridpartition
-function fis = gridpartition_fis(training_data,training_targets)
-opts = genfisOptions('GridPartition');
-opts.NumMembershipFunctions = 3;
-fis = genfis(training_data,training_targets,opts);
+%evalute algorithm
+function evaluate_algorithm(fis)
+global testing_data;
+global testing_targets;
+global figure_index;
+trainFis = train_algorithm(fis,100);
+output = round(evalfis(trainFis,testing_data));
+[success_rate,successful] = get_success_results(output);
+plotClassification(output);
+plotInputs(fis);
 end
 
-
-%substractive clustering
-function fis = substractive_fis(training_data,training_targets)
-opts= genfisOptions('SubtractiveClustering');
-fis= genfis(training_data,training_targets,opts);
+%train algorithm
+function trainfis = train_algorithm(fis,epochs)
+global testing_set;
+global training_set;
+[trainfis, trainError, stepSize, checkFis, checkError] = ...
+    anfis(training_set, fis,epochs,  [], testing_set);
 end
 
-%run algorithm
-function trainfis = run_algorithm(training_set,algo,epochs,testing_set)
-[trainFis, trainError, stepSize, checkFis, checkError] = ...
-    anfis(training_set, algo,100,  [], testing_set);
-end
 
 %plot inputs
 function plotInputs(fis)
+global figure_index;
+figure_index = figure_index+1;
+figure(figure_index);
 [x,mf] = plotmf(fis,'input',1);
 subplot(2,2,1)
 plot(x,mf)
@@ -82,15 +74,66 @@ xlabel('Πλάτος Πετάλων')
 end
 
 %success rate
-function [success_rate,successful] = get_success_results(output,testing_targets)
+function [success_rate,successful] = get_success_results(output)
+global testing_targets;
 successful = size(find((output == testing_targets) == 0), 1);
-success_rate = round((successful/75)*100)
+success_rate = round((successful/75)*100);
 end
 
 %plot classification
-function plotClassification(output,testing_targets)
+function plotClassification(output)
+global figure_index;
+figure_index = figure_index+1;
+figure(figure_index);
+global testing_targets;
 plot(output)
 hold on
 plot(testing_targets,'o')
 hold off
 end
+
+%fuzzy c-means
+function fis = fcm_fis()
+global training_data;
+global training_targets;
+opts = genfisOptions('FCMClustering');
+opts.NumClusters = 3;
+fis = genfis(training_data,training_targets,opts);
+end 
+
+%fuzzy c-means RUN
+function [success_rate,successful] = fcm()
+fis = fcm_fis();
+evaluate_algorithm(fis);
+end
+
+
+%gridpartition
+function fis = gridpartition_fis()
+global training_data;
+global training_targets;
+opts = genfisOptions('GridPartition');
+opts.NumMembershipFunctions = 3;
+fis = genfis(training_data,training_targets,opts);
+end
+
+%gridpartition RUN
+function [success_rate,successful] = gridpartition(figure_index)
+fis = gridpartition_fis();
+evaluate_algorithm(fis);
+end
+
+%substractive clustering
+function fis = substractive_fis()
+global training_data;
+global training_targets;
+opts= genfisOptions('SubtractiveClustering');
+fis= genfis(training_data,training_targets,opts);
+end
+
+%substractive clustering RUN
+function [success_rate,successful] = substractive(figure_index)
+fis = substractive_fis();
+evaluate_algorithm(fis);
+end
+
